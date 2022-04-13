6df9c2343cb5aff307ecd00a6446b9db5c044d74
SaharShriker <95046499+SaharShriker@users.noreply.github.com>
2022-04-13 13:12:28 +0300

    working on CustomerService and add smart Dialects

--- src/main/java/com/jb/coupon_system_spring/service/CustomerService.java
+++ src/main/java/com/jb/coupon_system_spring/service/CustomerService.java
@@ -1,15 +1,56 @@
 package com.jb.coupon_system_spring.service;
 
+import com.jb.coupon_system_spring.beans.Category;
+import com.jb.coupon_system_spring.beans.Coupon;
+import com.jb.coupon_system_spring.beans.Customer;
 import com.jb.coupon_system_spring.repository.CompanyRepo;
 import com.jb.coupon_system_spring.repository.CouponRepo;
 import com.jb.coupon_system_spring.repository.CustomerRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class CustomerService {
+public class CustomerService implements CustomerServiceInterFace {
     private final CompanyRepo companyRepo;
     private final CustomerRepo customerRepo;
     private final CouponRepo couponRepo;
+
+    private int customerId = 1; // for testing
+
+    @Override
+    public void purchaseCoupon(Coupon coupon) {
+
+    }
+
+    @Override
+    public List<Coupon> getCustomerCoupon() {
+
+        return null;
+    }
+
+    @Override
+    public List<Coupon> getCustomerCouponByCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public List<Coupon> getCustomerCouponByPrice(double price) {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomerDetails(int id) {
+        Optional<Customer> customer = customerRepo.findById(id);
+        if (customer.isPresent()){
+            return customer.get();
+        }
+        else {
+            System.out.println("no customer!");
+            return null;
+        }
+    }
 }
